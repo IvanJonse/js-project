da88994ba7c585c59a80c5d913f9cd319ab75756
ivkn31 <ivkn31@gmail.com>
2022-08-08 20:09:02 +0700

    feat: first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const slides = document.querySelectorAll('.slide')
+
+slides.forEach(slide => {
+    slide.addEventListener('click', () => {
+        removeActive()
+        slide.classList.add('active')
+        
+    })
+})
+
+function removeActive() {
+    slides.forEach(slide => {
+        slide
+            .classList
+            .remove('active')
+    })
+}
